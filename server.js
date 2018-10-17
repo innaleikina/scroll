53ecc4eeb1f4b475b35a8d79229da91eda99ecd6
Olga <oserenchenko@gmail.com>
2018-10-16 20:01:53 -0600

    auth

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const  routes = require('./routes');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +17,33 @@ if(process.env.NODE_ENV === "production") {
 app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/scroll");
+
+////SETTING UP PASSPORT START---------------------------------------------------
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(userId, done) {
+  User.findById(userId, (err, user) => done(err, user));
+});
+
+const local = new LocalStrategy((username, password, done) => {
+  User.findOne({ username })
+    .then(user => {
+      if (!user || !user.validPassword(password)) {
+        done(null, false, { message: "Invalid username/password" });
+      } else {
+        done(null, user);
+      }
+    })
+    .catch(e => done(e));
+});
+passport.use("local", local);
+
+////SETTING UP PASSPORT END---------------------------------------------------
 
 app.listen(PORT, () => {
   console.log('Running on port:', PORT);
